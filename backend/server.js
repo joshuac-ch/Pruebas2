106b53f75564f6c32d805860257049b9a4782b3c
joshua <condorenajoshua98@gmail.com>
2025-01-16 13:25:51 -0500

    se agrego la indexacion de Formulario

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,25 @@ app.get("/",(req,res)=>{
   })
 
 })
+app.post("/insert",(req,res)=>{
+  const {nombre}=req.body
+  if(!nombre){
+    res.status(404).json({message:"todos los campos deben estar llenos"})
+  }
+  const queryInsertar="INSERT INTO usuarios (nombre) VALUES (?)"
+  conextion.query(queryInsertar,nombre,(err,result)=>{
+    if(err){
+      console.log("hubo un error en la insercion",err.message)
+      res.status(500).json({message:"error 500 no se la conexion"})
+    }else{
+      console.log("datos insertados correctamentes",{id:result.insertId,nombre});
+      res.status(201).json({
+       message:"datos ingresados",
+       regustri:{id:result.insertId,nombre}
+      })
+    }
+  })
+})
 const port=3006
 app.listen(port,()=>{
   console.log(`ingrese al siguiente link http://localhost:${port}`)
